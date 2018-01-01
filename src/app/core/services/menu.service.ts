6a2052088d0068bc5e67272683c16205f1c58fbf
robin <robin_v@126.com>
2018-01-01 11:08:45 +0800

    chore: update project

--- src/app/core/services/menu.service.ts
+++ src/app/core/services/menu.service.ts
@@ -10,7 +10,7 @@ export class MenuService {
 
   getMenu() {
     return new Promise((resolve, reject) => {
-      this.http.get('http://www.easy-mock.com/mock/5a011b579d3ceb4a354379db/menu')
+      this.http.get('https://www.easy-mock.com/mock/5a011b579d3ceb4a354379db/menu')
         .subscribe(result => {
           this.setting.setMenuStatus(true);
           resolve(result);
